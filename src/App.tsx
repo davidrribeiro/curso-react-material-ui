cfee16c583d15417035b86c09931d2c8e11b16ad
David Ribeiro <daviddev.code@gmail.com>
2022-04-05 23:24:56 -0300

    add react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {}
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/dashboard" element={<p>Dashboard</p>} />
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
+  );
+};
